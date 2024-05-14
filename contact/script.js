d01fd80c6fd8ca86c8df501136481658e2b2b69c
Charlie <charlie_cwv@hotmail.com>
2024-05-14 17:52:55 -0300

    mudando pastas

--- contact/script.js
+++ contact/script.js
@@ -1,16 +1,16 @@
-// Lista de clínicas (pode ser substituída por dados dinâmicos do servidor)
+
 const listaDeClinicas = [
     { nome: "Clínica 1", nota: "-", estrelas: "-", link: "perfil_empresa.html" },
     { nome: "Clínica 2", nota: "-", estrelas: "-", link: "perfil_empresa.html" },
     { nome: "Clínica 3", nota: "-", estrelas: "-", link: "perfil_empresa.html" },
     { nome: "Clínica 4", nota: "-", estrelas: "-", link: "perfil_empresa.html" }
-    // Adicione mais clínicas conforme necessário
+
 ];
 
-// Índice inicial das clínicas a serem exibidas
+
 let startIndex = 0;
 
-// Função para criar o HTML de uma clínica
+
 function criarClinicaHTML(clinica) {
     return `
     <section class="clinica">
@@ -23,28 +23,28 @@ function criarClinicaHTML(clinica) {
     `;
 }
 
-// Função para adicionar as clínicas ao container
+
 function adicionarClinicasAoContainer() {
     const clinicasContainer = document.getElementById('clinicas-container');
-    clinicasContainer.innerHTML = ''; // Limpa o conteúdo atual
+    clinicasContainer.innerHTML = ''; 
     listaDeClinicas.forEach(clinica => {
         const clinicaHTML = criarClinicaHTML(clinica);
         clinicasContainer.innerHTML += clinicaHTML;
     });
 }
 
-// Exibe as primeiras 4 clínicas ao carregar a página
+
 window.onload = function() {
     adicionarClinicasAoContainer();
 };
 
-// Função para exibir as próximas 4 clínicas
+
 function nextClinics() {
     startIndex += 4;
     adicionarClinicasAoContainer();
 }
 
-// Função para exibir as clínicas anteriores
+
 function previousClinics() {
     startIndex -= 4;
     if (startIndex < 0) {
